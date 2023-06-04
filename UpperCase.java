46436ebb5fe8e7712adc88faab8e87c91ddefac1
sampleAdi <128214216+sampleAdi@users.noreply.github.com>
2023-06-04 18:18:24 +0530

    Create UpperCase.java

---
+++ UpperCase.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class UpperCase {
+
+    public static String upper(String str) {
+        StringBuilder sb = new StringBuilder("");
+        sb.append(Character.toUpperCase(str.charAt(0)));
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == ' ' && i < str.length()) {
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            } else {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "hello i'am aditya tiwari";
+        System.out.println(upper(str));
+    }
+}
